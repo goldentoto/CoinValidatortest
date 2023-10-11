e8199c214bdfc6bf2c70d3d60cb7dfea12990cf8
Toto <109338896+goldentoto@users.noreply.github.com>
2023-10-11 14:44:40 -0600

    Update SinkStub.java

--- SinkStub.java
+++ SinkStub.java
@@ -7,7 +7,14 @@ import com.tdc.Sink;
 public class SinkStub<T> implements Sink<T> {
 		
 	public void receive(T cash) throws CashOverloadException, DisabledException {
+		private T lastReceivedCash;
 		
+		public void receive(T cash) throws CashOverloadException, DisabledException {
+			if (!hasSpace()) {
+	            throw new CashOverloadException("The sink is overloaded.");}
+	        lastReceivedCash = cash;
+
+	}
 	}
 	
 	public boolean hasSpace() {
