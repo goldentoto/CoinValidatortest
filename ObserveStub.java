8176be222d4d47996376a8fcfca75245b4d9c855
Toto <109338896+goldentoto@users.noreply.github.com>
2023-10-11 15:09:04 -0600

    Update ObserveStub

--- ObserveStub.java
+++ ObserveStub.java
@@ -1,11 +1,50 @@
-/**
- * 
- */
+
 package com.thelocalmarketplace.hardware.test;
+import java.math.BigDecimal;
 
 /**
  * 
  */
-class ObserveStub {
+import com.tdc.IComponent;
+import com.tdc.IComponentObserver;
+import com.tdc.coin.*;
+
+class ObserveStub implements CoinValidatorObserver {
+	public boolean validCoin = false;
+
+	@Override
+	public void enabled(IComponent<? extends IComponentObserver> component) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disabled(IComponent<? extends IComponentObserver> component) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnedOn(IComponent<? extends IComponentObserver> component) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnedOff(IComponent<? extends IComponentObserver> component) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void validCoinDetected(AbstractCoinValidator validator, BigDecimal value) {
+		validCoin = true;
+	}
+
+	@Override
+	public void invalidCoinDetected(AbstractCoinValidator validator) {
+		validCoin = false;
+		
+	}
 
 }
